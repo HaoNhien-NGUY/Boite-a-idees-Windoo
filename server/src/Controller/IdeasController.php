86821e9f2da80c1123fb8c552a291a8e647ef6c6
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-08-03 19:02:05 +0200

    ideas route done

---
+++ server/src/Controller/IdeasController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Faker;
+
+class IdeasController extends AbstractController
+{
+    /**
+     * @Route("/api/ideas", name="ideas", methods="GET")
+     */
+    public function index()
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $nbIdeas = $faker->numberBetween(10, 50);
+        $ideas = [];
+
+        for($i = 0; $i < $nbIdeas; $i++) {
+            $ideas[] = [
+                "id" => $i, 
+                "title" => $faker->text(25), "createdAt" => $faker->dateTimeBetween('-6 months', 'now', 'Europe/Paris'),
+                "author" => $faker->name,
+                "score" => $faker->numberBetween(0, 50)
+            ];
+        }
+
+        return $this->json($ideas , 200, []);
+    }
+}
